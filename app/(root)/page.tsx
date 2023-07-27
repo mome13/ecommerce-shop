bd30b266aabc61460aa034a158b9c0d631785af4
mehran13mome <mehran13mome@gmail.com>
2023-07-27 23:53:36 +0330

    Initial commit

---
+++ app/(root)/page.tsx
@@ -0,0 +1,11 @@
+import { Button } from '@/components/ui/button';
+
+export default function page() {
+  return (
+    <div className='flex h-screen justify-center items-center'>
+      <Button size='default' variant='outline'>
+        Mehran
+      </Button>
+    </div>
+  );
+}
