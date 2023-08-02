14196fe4fbaf2506a9751a66328262628f2ea248
mehran13mome <mehran13mome@gmail.com>
2023-08-02 14:14:35 +0330

    amend main layout
    -relocate providers to body

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import type { Metadata } from 'next';
 import { ClerkProvider } from '@clerk/nextjs';
 import { Inter } from 'next/font/google';
 import { ModalProvider } from '@/providers/modal-provider';
+import { ToastProvider } from '@/providers/toast-provider';
 
 const inter = Inter({ subsets: ['latin'] });
 
@@ -18,9 +19,12 @@ export default function RootLayout({
 }) {
   return (
     <ClerkProvider>
-      <ModalProvider/>
       <html lang='en'>
-        <body className={inter.className}>{children}</body>
+        <body className={inter.className}>
+          <ToastProvider />
+          <ModalProvider />
+          {children}
+        </body>
       </html>
     </ClerkProvider>
   );
