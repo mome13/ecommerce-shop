feb239cafc2e4e349becd17aa57464ed1b350384
mehran13mome <mehran13mome@gmail.com>
2023-08-02 14:13:55 +0330

    Add modal for creating store

--- components/modals/store-modal.tsx
+++ components/modals/store-modal.tsx
@@ -43,7 +43,9 @@ export const StoreModal = () => {
     try {
       setLoading(true);
       const response = await axios.post('/api/stores', values);
-      window.location.assign(`/${response.data.id}`);
+      toast.success('Store created.');
+
+      // window.location.assign(`/${response.data.id}`);
     } catch (error) {
       toast.error('Something went wrong');
     } finally {
